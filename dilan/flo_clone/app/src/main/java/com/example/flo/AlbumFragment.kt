b2468481c9d720cb54b4cac780b0ec84540d6743
Jangwoo <wkdxod321@gmail.com>
2022-03-31 11:25:02 +0900

    add ViewPager, TabLayout

--- dilan/flo_clone/app/src/main/java/com/example/flo/AlbumFragment.kt
+++ dilan/flo_clone/app/src/main/java/com/example/flo/AlbumFragment.kt
@@ -4,12 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.flo.databinding.FragmentAlbumBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class AlbumFragment : Fragment() {
     lateinit var binding: FragmentAlbumBinding
+
+    private val information = arrayListOf("수록곡", "상세정보", "영상")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -17,24 +19,20 @@ class AlbumFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = FragmentAlbumBinding.inflate(inflater, container, false)
+
         binding.albumBtnBackIv.setOnClickListener {
             (context as MainActivity).supportFragmentManager.beginTransaction()
                 .replace(R.id.main_frm, HomeFragment()).commitAllowingStateLoss()
 
         }
 
-        binding.songLilacLayoutCl.setOnClickListener {
-            Toast.makeText(activity, "LILAC", Toast.LENGTH_SHORT).show()
-        }
 
-        binding.songFluLayoutCl.setOnClickListener {
-            Toast.makeText(activity, "FLU", Toast.LENGTH_SHORT).show()
-        }
+        val albumAdapter = AlbumVPAdapter(this)
+        binding.albumContentVp.adapter = albumAdapter
+        TabLayoutMediator(binding.albumContentTb, binding.albumContentVp) { tab, position ->
+            tab.text = information[position]
+        }.attach()
 
-
-        binding.songCoinLayoutCl.setOnClickListener {
-            Toast.makeText(activity, "COIN", Toast.LENGTH_SHORT).show()
-        }
         return binding.root
     }
 
